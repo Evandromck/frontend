48ff80b1134d93626fc691af91ad30dc39e9676f
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-04-24 21:42:22 -0300

    deploy Manu inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './componentes/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './componentes/template/footer/footer.component';
+import { NavComponent } from './componentes/template/nav/nav.component';
+
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
